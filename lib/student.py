304b06a208f4f2e93aef32a18617839d8393100c
Lincoln Muraguri <lincolnmuraguri93@gmail.com>
2023-05-20 11:47:02 +0300

    Done

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 from user import User
+from teacher import Teacher
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = []
+
+    def learn(self, str):
+        return self.knowledge.append(str)
